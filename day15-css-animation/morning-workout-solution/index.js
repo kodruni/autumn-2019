da4e56a60c071782c4b293bf811fa350e6825cec
Martin Podloucký <podlouckymartin@gmail.com>
2019-09-27 10:41:38 +0200

    Day 15 morning workout solution.

---
+++ day15-css-animation/morning-workout-solution/index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const padlock = document.querySelector('#padlock');
+  const message = document.querySelector('#message');
+  
+  padlock.addEventListener('mouseenter', () => {
+    message.classList.remove('hidden');  
+  });
+
+  padlock.addEventListener('mouseleave', () => {
+    message.classList.add('hidden');
+  });
+});
