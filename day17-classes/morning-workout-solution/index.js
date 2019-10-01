a160476cda9f5500190b3f946d3644956dfe11a6
Martin Podloucký <podlouckymartin@gmail.com>
2019-10-01 11:01:02 +0200

    Day 17 morning workout solution.

---
+++ day17-classes/morning-workout-solution/index.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const chat = document.querySelector('#chat');
+
+  for(const msg of conversation) {
+    const name = msg.name.toLowerCase();
+    chat.innerHTML += (
+      `<div class="message ${msg.side}">
+          <div class="head ${name}">
+          <img src="img/${name}.png"/>
+        </div>
+        <div class="text">${msg.text}</div>
+       </div>`
+    );
+  };
+});
