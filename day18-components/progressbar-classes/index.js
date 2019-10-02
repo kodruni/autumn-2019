d5d9fb282e421a2bdd662160914c86a6f55006d2
Martin Podloucký <podlouckymartin@gmail.com>
2019-10-02 11:17:53 +0200

    Progress bar demo with classes.

--- day18-components/progressbar-classes/index.js
+++ day18-components/progressbar-classes/index.js
@@ -1,14 +1,13 @@
-let value = 1;
-const maxValue = 20;
+const bar = new ProgressBar(10, 30);
 
 const update = () => {
   const counter = document.querySelector('#counter');
-  counter.textContent = `${value}`;
+  counter.textContent = `${bar.value}`;
   const maximum = document.querySelector('#maximum');
-  maximum.textContent = `${maxValue}`;
+  maximum.textContent = `${bar.max}`;
   const knob = document.querySelector('#knob');
-  const step = 100 / maxValue;
-  knob.style.width = `${value * step}%`;
+  const step = 100 / bar.max;
+  knob.style.width = `${bar.value * step}%`;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -16,17 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const plusBtn = document.querySelector('#plus');
   plusBtn.addEventListener('click', () => {
-    if (value < maxValue) {
-      value++;
-      update();
-    }
+    bar.increment();
+    update();
   });
 
   const minusBtn = document.querySelector('#minus');
   minusBtn.addEventListener('click', () => {
-    if (value > 0) {
-      value--;
-      update();
-    }
+    bar.decrement();
+    update();
   });
 });
