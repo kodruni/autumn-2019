eede170a2f96c0ff436fa69ce5876402d634d82e
j_polak <jan.polak@onmyway.cz>
2019-11-04 10:39:48 +0100

    day 41 morning workout

---
+++ day41-files-uploads/workout/src/components/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+import Search from './Search.jsx';
+import ProductList from './ProductList.jsx';
+
+export default class App extends React.Component {
+
+    render() {
+        return (
+            <div id="page">
+
+                <Header />
+
+                <div id="main">
+                    
+                    <Search />
+
+                    <div className="content">
+                        <h1>Products</h1>
+
+                        <ProductList />
+
+                    </div>
+
+                </div>
+
+                <Footer />
+                
+
+            </div>
+        )
+    }
+}
